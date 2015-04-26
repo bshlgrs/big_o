f4c59247537f975e6c647cb2aa17074f7ad77efb
Buck Shlegeris <bshlegeris@paypal.com>
2015-04-26 00:01:50 -0700

    more ruby extensions

--- ruby/extensions/my_silly_struct.c
+++ ruby/extensions/my_silly_struct.c
@@ -1,8 +1,21 @@
 #include <ruby.h>
 #include "extconf.h"
 
+
+
+VALUE my_method(VALUE self, VALUE num) {
+  unsigned long myVal = FIX2LONG(num);
+  printf("the int is %lu?\n", myVal);
+
+  return LONG2FIX(num);
+}
+
 void Init_my_silly_struct()
 {
   int state;
-  rb_eval_string_protect("puts 'Hello, world!'", &state);
+  // rb_eval_string_protect("puts 'Hello, world!'", &state);
+
+  VALUE mySillyStruct = rb_define_class("MySillyStruct", rb_cObject);
+  rb_define_method(mySillyStruct, "my_method", my_method, 1);
 }
+
---
+++ ruby/priority_queue.c
@@ -0,0 +1,30 @@
+
+
+
+VALUE getPriority(VALUE self, VALUE item) {
+  long item_int = FIX2LONG(item);
+
+  // this is made up
+  magic ms = getStructFromValue(self);
+
+  hashMap1Node *node1 = hashMap__get(ms.hashMap1, item_int);
+
+  return DBL2NUM(hashMapNode__getPriority(ms.)) ???
+  // do this in `get`
+  // if (hashMap1Node == NULL) {
+  //   rb_raise_exception("index not found")
+  // } else {
+  //   return DBL2NUM(node1.priority);
+  // }
+}
+
+VALUE adjustPriority(VALUE self, VALUE item, VALUE priority) {
+  long item_int = FIX2LONG(item);
+  double priority_double = RFLOAT_VALUE(priority);
+
+  magic ms = getStructFromValue(self);
+
+  hashMap1Node *node1 = hashMap__get(ms.hashMap1, item_int);
+
+
+}
